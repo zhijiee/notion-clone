ec00a336477f714c0a8aecfe97cdde38f4acd06d
zhijie <zhijie12@gmail.com>
2023-10-22 16:16:51 +0800

    Added Login Button in Navbar

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -3,6 +3,7 @@
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
 import { Logo } from "./logo";
+import { Button } from "@/components/ui/button";
 
 export const NavBar = () => {
     const scrolled = useScrollTop();
@@ -12,6 +13,9 @@ export const NavBar = () => {
             scrolled && "border-b shadow-sm"
         )}>
             <Logo />
+            <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+                <Button> Login </Button>
+            </div>
         </div>
     )
 }
