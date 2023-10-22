5e9c1ba51161699a1db3c3add66d06056808c87c
zhijie <zhijie12@gmail.com>
2023-10-22 15:10:21 +0800

    Removed original page and added marketing page.
    Added heading and heroes

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { Heading } from "./_components/heading";
+import { Heroes } from "./_components/heroes";
+
+const MarketingPage = () => {
+  return (
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center 
+        md:justify-start text-center gap-y-8 flex-1 px-6 pd-10">
+          <Heading />
+          <Heroes />
+
+      </div>
+    </div>
+  );
+}
+
+export default MarketingPage;
